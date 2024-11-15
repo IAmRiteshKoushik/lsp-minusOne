dfc8037456c0558c9e316554bd136152d521feef
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-11-15 16:38:53 +0530

    Add: Decode message for LSP

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 // We need to encode and send the message to our LSP so that it can decode,
@@ -15,4 +18,27 @@ func EncodeMessage(msg any) string {
 	}
 
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+// The bytes.Cut() function accepts two byte-slices and works as a partition
+// funcion by identifying the first occurance of the subsequence of bytes in
+// the msg. and partitioning around it. This is also the reason why we are using
+// []byte as type of msg instead of doing strings. Also, []byte is faster to
+// work with as compared to strings or chars
+func DecodeMessage(msg []byte) (int, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, errors.New("Did not find a separator")
+	}
+
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, err
+	}
+
+	// TODO: Use content somewhere at a later point
+	_ = content
+
+	return contentLength, nil
 }
--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -16,3 +16,16 @@ func TestEncode(t *testing.T) {
 		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
 	}
 }
+
+func TestDecode(t *testing.T) {
+	incomingMessage := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
+	contentLength, err := rpc.DecodeMessage([]byte(incomingMessage))
+	if err != nil {
+		// If decoding did not work then error
+		t.Fatal(err)
+	}
+
+	if contentLength != 16 {
+		t.Fatalf("Expected: 16, Got: %d", contentLength)
+	}
+}
